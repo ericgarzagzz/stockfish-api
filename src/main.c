b97b62ad3310de70a0b053a31df9200321e9de8a
Eric Garza Gonzalez <ericgarzaroot@gmail.com>
2026-01-07 16:00:40 -0600

    Improve error handling on curl download

--- src/main.c
+++ src/main.c
@@ -28,40 +28,44 @@ int download_stockfish_executable() {
 
 	result = curl_global_init(CURL_GLOBAL_ALL);
 	if (result) {
-		fprintf(stderr, "Could not init curl\n");
+		fprintf(stderr, "Failed to initialize curl library\n");
 		return (int)result;
 	}
 
 	curl = curl_easy_init();
-	if (curl) {
-		FILE* stockfish_tar;
-
-		// TODO: Support Windows and MacOS as well (once cross-platform compilation is implemented)
-		char* download_url = "https://github.com/official-stockfish/Stockfish/releases/download/sf_17.1/stockfish-ubuntu-x86-64.tar";
-		curl_easy_setopt(curl, CURLOPT_URL, download_url);
-		curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
-		curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 0L);
-		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_cb);
-
-		static const char *stockfish_tar_filename = ".cache/stockfish.tar";
-
-		if (ensure_directory_exists(".cache") != 0) {
-			curl_easy_cleanup(curl);
-			curl_global_cleanup();
-			return -1;
-		}
-
-		stockfish_tar = fopen(stockfish_tar_filename, "wb");
-		if (stockfish_tar) {
-			curl_easy_setopt(curl, CURLOPT_WRITEDATA, stockfish_tar);
-
-			result = curl_easy_perform(curl);
-
-			fclose(stockfish_tar);
-		}
-
-		curl_easy_cleanup(curl);
+	if (!curl) {
+		fprintf(stderr, "Failed to initialize curl handle\n");
+		curl_global_cleanup();
+		return CURLE_FAILED_INIT;
 	}
+
+	FILE* stockfish_tar;
+
+	// TODO: Support Windows and MacOS as well (once cross-platform compilation is implemented)
+	char* download_url = "https://github.com/official-stockfish/Stockfish/releases/download/sf_17.1/stockfish-ubuntu-x86-64.tar";
+	curl_easy_setopt(curl, CURLOPT_URL, download_url);
+	curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+	curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 0L);
+	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_cb);
+
+	static const char *stockfish_tar_filename = ".cache/stockfish.tar";
+
+	if (ensure_directory_exists(".cache") != 0) {
+		curl_easy_cleanup(curl);
+		curl_global_cleanup();
+		return -1;
+	}
+
+	stockfish_tar = fopen(stockfish_tar_filename, "wb");
+	if (stockfish_tar) {
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, stockfish_tar);
+
+		result = curl_easy_perform(curl);
+
+		fclose(stockfish_tar);
+	}
+
+	curl_easy_cleanup(curl);
 
 	curl_global_cleanup();
 
